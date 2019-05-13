f02b8877afe7188ee5051be6d2300f60787935a4
Anders Kvamme Felde <anderskfelde@hotmail.com>
2019-05-13 08:08:49 +0200

    utforsk

--- elements/echo_innlegg.php
+++ elements/echo_innlegg.php
@@ -49,201 +49,205 @@ function echo_innlegg( $ting ) {
         timestampdiff(WEEK, tid, now()) as week,
         timestampdiff(YEAR, tid, now()) as year
         $ting";
-    
-    echo "$sql";
+
 
     $resultat = $kobling->query( $sql );
 
     echo "<div class='posts'>";
 
-    while ( $rad = $resultat->fetch_assoc() ) {
+    if ( !mysqli_num_rows( $resultat ) == 0 ) {
 
-        $username = $rad[ "brukernavn" ];
+        while ( $rad = $resultat->fetch_assoc() ) {
 
-        $bilde = $rad[ "bilde" ];
-        $innlegg_id = $rad[ "innlegg_id" ];
-        $tekst = $rad["tekst"];
-        $second = $rad[ "second" ];
-        $minute = $rad[ "minute" ];
-        $hour = $rad[ "hour" ];
-        $week = $rad[ "week" ];
-        $day = $rad[ "day" ];
-        $month = $rad[ "month" ];
-        $year = $rad[ "year" ];
+            $username = $rad[ "brukernavn" ];
 
-
-        echo "<div class='post'>";
-
-        echo "<div class='title'>";
-        echo "<h2>@$username</h2>";
+            $bilde = $rad[ "bilde" ];
+            $innlegg_id = $rad[ "innlegg_id" ];
+            $tekst = $rad[ "tekst" ];
+            $second = $rad[ "second" ];
+            $minute = $rad[ "minute" ];
+            $hour = $rad[ "hour" ];
+            $week = $rad[ "week" ];
+            $day = $rad[ "day" ];
+            $month = $rad[ "month" ];
+            $year = $rad[ "year" ];
 
 
-        echo "<h2>";
-        if ( $second > 59 ) {
-            if ( $minute > 59 ) {
-                if ( $hour > 23 ) {
-                    if ( $day > 6 ) {
-                        if ( $month == 0 ) {
+            echo "<div class='post'>";
 
-                            echo "$week";
+            echo "<div class='title'>";
+            echo "<h2>@$username</h2>";
 
-                        } elseif ( $month > 11 ) {
 
-                            echo "$year år siden";
+            echo "<h2>";
+            if ( $second > 59 ) {
+                if ( $minute > 59 ) {
+                    if ( $hour > 23 ) {
+                        if ( $day > 6 ) {
+                            if ( $month == 0 ) {
+
+                                echo "$week";
+
+                            } elseif ( $month > 11 ) {
+
+                                echo "$year år siden";
+
+                            } else {
+                                echo "$month måneder siden";
+                            }
 
                         } else {
-                            echo "$month måneder siden";
+                            echo "$day dager siden";
                         }
-
                     } else {
-                        echo "$day dager siden";
+                        echo "$hour t siden";
                     }
                 } else {
-                    echo "$hour t siden";
+                    echo "$minute min siden";
                 }
             } else {
-                echo "$minute min siden";
+                echo "$second s siden";
             }
-        } else {
-            echo "$second s siden";
-        }
-        echo "</h2></div>";
+            echo "</h2></div>";
 
 
-        $sql_kat = "select kategori from kategori
+            $sql_kat = "select kategori from kategori
           where innlegg_id = $innlegg_id";
 
-        $resultat_kat = $kobling->query( $sql_kat );
+            $resultat_kat = $kobling->query( $sql_kat );
 
-        $sql_vote = "select 
+            $sql_vote = "select 
                     IFNULL(sum(vote=1), 0) as up, 
                     IFNULL(sum(vote=0), 0) as down 
                     from voted 
                     where innlegg_id = $innlegg_id ";
-        
-        $sql_kom = "select 
+
+            $sql_kom = "select 
                     count(*) as komCon
                     from kommentar
                     where innlegg_id = $innlegg_id ";
 
 
-            $rad_vote = ($kobling->query($sql_vote))->fetch_assoc();
-            $rad_kom = ($kobling->query($sql_kom))->fetch_assoc();
+            $rad_vote = ( $kobling->query( $sql_vote ) )->fetch_assoc();
+            $rad_kom = ( $kobling->query( $sql_kom ) )->fetch_assoc();
 
             $upcount = $rad_vote[ "up" ];
             $downcount = $rad_vote[ "down" ];
-            $commentcount = $rad_kom["komCon"];
-            
-
-        echo "<div class='categorier'>";
-        while ( $rad_kat = $resultat_kat->fetch_assoc() ) {
-            $kategori = $rad_kat[ "kategori" ];
-
-            echo "<span>#$kategori</span>";
-
-        }
-        echo "</div>";
+            $commentcount = $rad_kom[ "komCon" ];
 
 
-        echo "<img src='images/innlegg_images/$bilde'><br>";
-        echo "<p class='tekst'>$tekst</p>";
+            echo "<div class='categorier'>";
+            while ( $rad_kat = $resultat_kat->fetch_assoc() ) {
+                $kategori = $rad_kat[ "kategori" ];
+
+                echo "<span>#$kategori</span>";
+
+            }
+            echo "</div>";
 
 
-        if ( isset( $_SESSION[ "bruker_id" ] ) ) {
+            echo "<img src='images/innlegg_images/$bilde'><br>";
+            echo "<p class='tekst'>$tekst</p>";
 
-            $bruker_id = $_SESSION[ "bruker_id" ];
 
-            $upvoted = false;
-            $downvoted = false;
+            if ( isset( $_SESSION[ "bruker_id" ] ) ) {
 
-            if ( isset( $bruker_vote_arr ) ) {
-                if ( in_array( $innlegg_id, $bruker_vote_arr ) ) {
-                    $innlegg_vote = ( array_search( $innlegg_id, $bruker_vote_arr ) ) + 1;
+                $bruker_id = $_SESSION[ "bruker_id" ];
 
-                    $bruker_vote = $bruker_vote_arr[ "$innlegg_vote" ];
+                $upvoted = false;
+                $downvoted = false;
 
-                    switch ( $bruker_vote ) {
-                        case 0:
+                if ( isset( $bruker_vote_arr ) ) {
+                    if ( in_array( $innlegg_id, $bruker_vote_arr ) ) {
+                        $innlegg_vote = ( array_search( $innlegg_id, $bruker_vote_arr ) ) + 1;
 
-                            $func_type_up = "upd";
-                            $func_type_down = "del";
-                            $downvoted = true;
-                            break;
+                        $bruker_vote = $bruker_vote_arr[ "$innlegg_vote" ];
 
-                        case 1:
+                        switch ( $bruker_vote ) {
+                            case 0:
 
-                            $func_type_up = "del";
-                            $func_type_down = "upd";
-                            $upvoted = true;
-                            break;
+                                $func_type_up = "upd";
+                                $func_type_down = "del";
+                                $downvoted = true;
+                                break;
+
+                            case 1:
+
+                                $func_type_up = "del";
+                                $func_type_down = "upd";
+                                $upvoted = true;
+                                break;
+
+                        }
+
+                    } else {
+
+                        $func_type_up = "norm";
+                        $func_type_down = "norm";
 
                     }
 
                 } else {
-
                     $func_type_up = "norm";
                     $func_type_down = "norm";
-
                 }
+                echo "<div class='postinfo'>";
 
+                echo "<div>";
+                echo "<div>";
+                if ( $upvoted == true ) {
+                    echo "<button value='$func_type_up' onclick=" . '"' . "vote(1,$bruker_id,$innlegg_id)" . '"' . " class='Material icon upvoted' id='upVote$innlegg_id'>arrow_upward</button>";
+                } else {
+                    echo "<button value='$func_type_up' onclick=" . '"' . "vote(1,$bruker_id,$innlegg_id)" . '"' . " class='Material icon' id='upVote$innlegg_id'>arrow_upward</button>";
+                }
+                echo "<p id='ucount$innlegg_id' class='count'>$upcount</p>";
+                echo "</div>";
+                echo "<div>";
+                if ( $downvoted == true ) {
+                    echo "<button value='$func_type_down' onclick=" . '"' . "vote(0,$bruker_id,$innlegg_id)" . '"' . " class='Material icon downvoted' id='downVote$innlegg_id'>arrow_downward</button>";
+                } else {
+                    echo "<button value='$func_type_down' onclick=" . '"' . "vote(0,$bruker_id,$innlegg_id)" . '"' . " class='Material icon' id='downVote$innlegg_id'>arrow_downward</button>";
+                }
+                echo "<p id='dcount$innlegg_id' class='count'>$downcount</p>";
+                echo "</div>";
+                echo "</div>";
             } else {
-                $func_type_up = "norm";
-                $func_type_down = "norm";
+
+                echo "<div class='postinfo'>";
+                echo "<div>";
+                echo "<div>";
+                $new_page = '"logg_inn"';
+                echo "<button onclick='redir($new_page)' class='Material icon'>arrow_upward</button>";
+                echo "<p id='ucount$innlegg_id' class='count'>$upcount</p>";
+                echo "</div>";
+                echo "<div>";
+                echo "<button onclick='redir($new_page)' class='Material icon'>arrow_downward</button>";
+                echo "<p id='dcount$innlegg_id' class='count'>$downcount</p>";
+                echo "</div>";
+                echo "</div>";
+
+
             }
-            echo "<div class='postinfo'>";
+            echo "<div>";
+            echo "<a href='#' class='Material icon'>share</a>";
+            echo "</div>";
 
             echo "<div>";
-            echo "<div>";
-            if ( $upvoted == true ) {
-                echo "<button value='$func_type_up' onclick=" . '"' . "vote(1,$bruker_id,$innlegg_id)" . '"' . " class='Material icon upvoted' id='upVote$innlegg_id'>arrow_upward</button>";
-            } else {
-                echo "<button value='$func_type_up' onclick=" . '"' . "vote(1,$bruker_id,$innlegg_id)" . '"' . " class='Material icon' id='upVote$innlegg_id'>arrow_upward</button>";
-            }
-            echo "<p id='ucount$innlegg_id' class='count'>$upcount</p>";
+            echo "<p class='count'>$commentcount</p>";
+            echo "<a href='#' class='Material icon'>short_text</a>";
+            echo "<p class='count hidemobile'>Kommentarer</p>";
             echo "</div>";
-            echo "<div>";
-            if ( $downvoted == true ) {
-                echo "<button value='$func_type_down' onclick=" . '"' . "vote(0,$bruker_id,$innlegg_id)" . '"' . " class='Material icon downvoted' id='downVote$innlegg_id'>arrow_downward</button>";
-            } else {
-                echo "<button value='$func_type_down' onclick=" . '"' . "vote(0,$bruker_id,$innlegg_id)" . '"' . " class='Material icon' id='downVote$innlegg_id'>arrow_downward</button>";
-            }
-            echo "<p id='dcount$innlegg_id' class='count'>$downcount</p>";
-            echo "</div>";
-            echo "</div>";
-        } else {
-            
-            echo "<div class='postinfo'>";
-            echo "<div>";
-            echo "<div>";
-            $new_page = '"logg_inn"';
-            echo "<button onclick='redir($new_page)' class='Material icon'>arrow_upward</button>";
-            echo "<p id='ucount$innlegg_id' class='count'>$upcount</p>";
-            echo "</div>";
-            echo "<div>";
-            echo "<button onclick='redir($new_page)' class='Material icon'>arrow_downward</button>";
-             echo "<p id='dcount$innlegg_id' class='count'>$downcount</p>";
-            echo "</div>";
-            echo "</div>";
-            
 
+            echo "</div>";
+
+            echo "</div>";
         }
-        echo "<div>";
-        echo "<a href='#' class='Material icon'>share</a>";
         echo "</div>";
-
-        echo "<div>";
-        echo "<p class='count'>$commentcount</p>";
-        echo "<a href='#' class='Material icon'>short_text</a>";
-        echo "<p class='count hidemobile'>Kommentarer</p>";
-        echo "</div>";
-
-        echo "</div>";
-
-        echo "</div>";
+    } else {
+        echo "Det er desverre ingen innlegg";
     }
     echo "</div>";
 }
-echo "</div>";
 
 
 
--- elements/utforsk_search.php
+++ elements/utforsk_search.php
@@ -1,34 +1,6 @@
-<form method="POST">
-        
-    <input name="search" list="kategorier_brukernavn" type="text">
-    <button onClick="redir('index')" type="submit"></button>
-        
-    <datalist id="kategorier_brukernavn">
-        <?php
-
-        $sql = "SELECT kategori, COUNT(kategori) as count FROM kategori group by kategori order by count desc;";
-
-        $resultat = $kobling->query( $sql );
-
-        while ( $rad = $resultat->fetch_assoc() ) {
-
-            $kategori = $rad[ "kategori" ];
-
-            echo "<option value='$kategori(kategori)'>";
-
-        }
-        $sql = "SELECT brukernavn FROM bruker order by brukernavn;";
-
-        $resultat = $kobling->query( $sql );
-
-        while ( $rad = $resultat->fetch_assoc() ) {
-
-            $brukernavn = $rad[ "brukernavn" ];
-
-            echo "<option value='$brukernavn(bruker)'>";
-
-        }
-
-        ?>
-    </datalist>
-    </form>
+<form autocomplete="off" class="search_form" method="post" action="utforsk.php">
+  <div class="autocomplete" style="width:300px;">
+    <input id="myInput" type="text" name="myCountry" placeholder="Søk...">
+  </div>
+  <input style="" type="submit">
+</form>
